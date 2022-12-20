0287ac76bf3c3b774db0ddfb5a70f7f9d452a76b
Roland Gorácz <roland@goracz.cloud>
2022-12-20 20:41:52 +0100

    Add OpenAPI to meta-data service

--- meta-service/src/main/java/com/goracz/metaservice/controller/ChannelMetadataController.java
+++ meta-service/src/main/java/com/goracz/metaservice/controller/ChannelMetadataController.java
@@ -3,6 +3,7 @@ package com.goracz.metaservice.controller;
 import com.goracz.metaservice.entity.ChannelMetadata;
 import com.goracz.metaservice.model.request.ChannelMetadataSearchRequest;
 import com.goracz.metaservice.service.ChannelMetadataService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class ChannelMetadataController {
     private final ChannelMetadataService channelMetadataService;
 
     @PostMapping("search")
+    @Operation(summary = "Search for channel meta-data by channel name")
     public ResponseEntity<Mono<ChannelMetadata>> getByChannelName(
             @RequestBody ChannelMetadataSearchRequest searchRequest) {
         return ResponseEntity.ok(this.channelMetadataService.getByChannelName(searchRequest.getChannelName()));
