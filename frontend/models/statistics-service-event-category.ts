ed6c0641db479f3e602e8a54d284d11722f32c41
Roland Gorácz <roland@goracz.cloud>
2023-01-11 21:11:13 +0100

    Revise MQ / WS event handling

---
+++ frontend/models/statistics-service-event-category.ts
@@ -0,0 +1,14 @@
+export enum StatisticsServiceEventCategory {
+    /**
+     * The TV channel history has changed
+     */
+    CHANNEL_HISTORY_CHANGED,
+    /**
+     * The app running in the foreground on the TV has changed
+     */
+    FOREGROUND_APP_CHANGED,
+    /**
+     * The uptime log of the TV has changed
+     */
+    UPTIME_LOG_CHANGED,
+}
