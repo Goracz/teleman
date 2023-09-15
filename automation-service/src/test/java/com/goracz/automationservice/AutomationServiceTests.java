9eeeed5aa2735e323bd1f5350cd8035f9a319312
Roland Gorácz <roland@goracz.cloud>
2023-09-15 21:11:59 +0200

    Disable empty tests

--- automation-service/src/test/java/com/goracz/automationservice/AutomationServiceTests.java
+++ automation-service/src/test/java/com/goracz/automationservice/AutomationServiceTests.java
@@ -1,12 +1,14 @@
 package com.goracz.automationservice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Disabled
 class AutomationServiceApplicationTests {
     @Test
     void contextLoads() {
